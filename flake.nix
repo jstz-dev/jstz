b4281257f0e66de393835c636443dd50ac9d067e
Alistair <ajo41@cam.ac.uk>
2023-07-31 14:14:13 +0100

    Add nix flake and shell

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [(import rust-overlay)];
+          };
+          jstz = pkgs.callPackage ./nix/jstz.nix {};
+        in {
+          packages = {
+            inherit jstz;
+            default = jstz.jstz_kernel;
+          };
+
+          # Rust dev environment
+          devShells.default = pkgs.mkShell {
+            NIX_CFLAGS_COMPILE = "-mcpu=generic";
+
+            buildInputs = with pkgs; [
+              alejandra
+              (rust-bin.stable."1.66.0".default.override {
+                targets = ["wasm32-unknown-unknown"];
+              })
+
+              rust-analyzer
+              wabt
+            ];
+          };
+        }
+      );
+}
---
+++ nix/jstz.nix
@@ -0,0 +1,49 @@
+{
+  pkgs,
+  makeRustPlatform,
+}: let
+  rustVersion = "1.66.1";
+
+  wasmTarget = "wasm32-unknown-unknown";
+
+  rustWithWasmTarget = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    targets = [wasmTarget];
+  };
+
+  rustPlatformWasm = makeRustPlatform {
+    cargo = rustWithWasmTarget;
+    rustc = rustWithWasmTarget;
+    llvmPackages = pkgs.llvm_16;
+  };
+
+  common = {
+    version = "0.1.0";
+    src = ../.;
+
+    cargoLock = {
+      lockFile = ../Cargo.lock;
+    };
+  };
+in {
+  jstz_core = pkgs.rustPlatform.buildRustPackage (common
+    // {
+      pname = "jstz_core";
+      cargoBuildFlags = "-p jstz_core";
+    });
+
+  jstz_kernel = rustPlatformWasm.buildRustPackage (common
+    // {
+      pname = "jstz_kernel";
+
+      NIX_CFLAGS_COMPILE = "-mcpu=generic";
+
+      buildPhase = ''
+        cargo build --release -p jstz_kernel --target=wasm32-unknown-unknown
+      '';
+
+      installPhase = ''
+        mkdir -p $out/lib
+        cp target/wasm32-unknown-unknown/release/*.wasm $out/lib/
+      '';
+    });
+}
