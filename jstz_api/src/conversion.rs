4b7c200056f4ed59178b725eac34b3c5cf5d2151
Sam Finch <sam.finch@trili.tech>
2023-08-24 11:56:02 +0100

    pause

--- jstz_api/src/conversion.rs
+++ jstz_api/src/conversion.rs
@@ -1,7 +1,5 @@
-
-
-
-use boa_engine::{JsValue, JsResult, JsError, Context};
+use boa_engine::{JsValue, JsResult, JsError, Context, JsNativeError};
+use derive_more::{Display, Error};
 
 pub trait FromJs : Sized{
   fn from_js(this : &JsValue, context : &mut Context) -> JsResult<Self>;
@@ -14,120 +12,18 @@ pub trait ToJsError {
   fn to_js_error(self, context: &mut Context) -> JsError;
 }
 
-
-mod from_js_ref {
-
-  pub struct Boxed;
-  pub struct Reference;
-  pub struct Sized;
-
-  pub trait FromJsRefStrategy
-  {
-    type JsRefStrategyReturn<'a, T : 'a> : 'a;
-  }
-
-  impl FromJsRefStrategy for Boxed{
-    type JsRefStrategyReturn<'a, T : 'a> = Box<T>;
-  }
-  impl FromJsRefStrategy for Reference{
-    type JsRefStrategyReturn<'a, T : 'a> = &'a T;
-  }
-  impl FromJsRefStrategy for Sized{
-    type JsRefStrategyReturn<'a, T : 'a> = T;
-  }
-
+#[derive(PartialEq, PartialOrd, Eq, Ord, Copy, Clone, Debug, Error, Display)]
+pub enum JsTypeError {
+  #[display(fmt = "Expected String")]
+  String,
 }
 
-/// this module effectively acts as a type level enum
-/// which is why it's CamelCase
-#[allow(non_snake_case)]
-pub mod JsRefStrategy {
-  pub use super::from_js_ref::{Boxed, Reference, Sized};
-}
-
-trait JsRefReturn<'a> : 'a{
-  type ReturnType: Sized + 'a;
-}
-
-use from_js_ref::FromJsRefStrategy;
-
-impl<'a, T> JsRefReturn<'a> for T
-where
-  T : FromJsRef<'a>,
-  <T::Strategy as FromJsRefStrategy>::JsRefStrategyReturn<'a,Self> : Sized {
-  type ReturnType = <<Self as FromJsRef<'a>>::Strategy as FromJsRefStrategy>::JsRefStrategyReturn<'a,Self> ;
-}
-
-trait FromJsRef<'a> : JsRefReturn<'a>
-{
-  type Strategy : FromJsRefStrategy;
-  fn from_js_ref (this: &'a JsValue, context : &mut Context) -> JsResult<<Self as JsRefReturn<'a>>::ReturnType>;
-}
-
-
-impl<'a> FromJsRef<'a> for u8 {
-  type Strategy = JsRefStrategy::Sized;
-  fn from_js_ref(this: &'a JsValue, context : &mut Context) -> JsResult<u8> {
-    this.to_uint8(context)
+impl ToJsError for JsTypeError {
+  fn to_js_error(self, context: &mut Context) -> JsError {
+    JsError::from_native(JsNativeError::typ().with_message(format!("{self}")))
   }
 }
-
-
-/*
-pub trait FromJsRef {
-  fn to_js_ref<'a>(&'a JsValue, context : &'b Context) -> &'a Self {
-
-  }
-}
-impl ToJs for i32 {
-  fn to_js(self, _context: &mut Context) -> JsResult<JsValue>{
-    Ok(JsValue::Integer(self))
-  }
-}
-impl FromJs for i32 {
-  fn from_js<'a, 'b>(this : &'a JsValue, context : &'b mut Context) -> JsResult<Self>
-  where Self: 'a {
-    this.to_i32(context)
-  }
-}
-#[repr(transparent)]
-struct JsStringInner(pub [u16]);
-unsafe impl TransparentWrapper<[u16]> for JsStringInner {}
-
-impl<'c> FromJs for &'c JsStringInner {
-  fn from_js<'a,'b>(this : &'a JsValue, _context : &'b mut Context) -> JsResult<Self>
-    where 'a : 'c
-  {
-    let JsValue::String(str) = this else {
-      return Err(JsError::from_native(JsNativeError::typ().with_message("expected string")))
-    };
-    Ok(TransparentWrapper::wrap_ref(str.as_slice()))
-  }
-}
-
-
-#[repr(transparent)]
-pub struct AnyError(Box<dyn Error>);
-impl AnyError {
-  pub fn new<T : Error + Clone + 'static>(source: T) -> Self {
-      AnyError(Box::new(source.clone()))
-  }
-}
-impl<E : Error + Clone + 'static> From<E> for AnyError {
-  fn from(source: E) -> Self {
-      AnyError::new(source)
-    }
-}
-fn err_to_js<T: Error + ?Sized> (err: &T) -> JsError {
-    let native = JsNativeError::error().with_message(format!("{err}"));
-    let native = match err.source() {
-      Some(cause) => native.with_cause(err_to_js(cause)),
-      None => native
-    };
-    JsError::from_native(native)
-}
-
-impl<T : ToJs, E: ToJsError> ToJs for Result<T, E> {
+impl<T: ToJs, E: ToJsError> ToJs for Result<T,E> {
   fn to_js(self, context: &mut Context) -> JsResult<JsValue> {
     match self {
       Ok(value) => value.to_js(context),
@@ -135,35 +31,19 @@ impl<T : ToJs, E: ToJsError> ToJs for Result<T, E> {
     }
   }
 }
-
-pub enum JsRefStrategy {
-  Convert, Direct
-}
-
-
-
-impl FromJs for i32 {
-  fn from_js(this : &JsValue, context: &mut Context) -> JsResult<Self> {
-    this.to_i32(context)
+impl<T: ToJs> ToJs for Option<T> {
+  fn to_js(self, context: &mut Context) -> JsResult<JsValue> {
+    match self {
+      Some(value) => value.to_js(context),
+      None => Ok(JsValue::default())
+    }
   }
 }
-impl ToJs for i32 {
-  fn to_js(self, _context: &mut Context) -> JsResult<JsValue> {
-    Ok(JsValue::Integer(self))
+impl<T: FromJs> FromJs for Option<T> {
+  fn from_js(this : &JsValue, context : &mut Context) -> JsResult<Self> {
+    match this {
+      JsValue::Null | JsValue::Undefined => Ok(None),
+      this => T::from_js(this, context).map(Some)
+    }
   }
 }
-
-macro_rules! from_js_clone {
-  ($($ident: ident),*) => {
-    $(
-      impl FromJs for $ident {
-        fn from_js(this: &JsValue, context: &mut Context) -> JsResult<Self> {
-          <& $ident as FromJs>::from_js(context).clone()
-        }
-      }
-    )*
-  }
-}
-
-
-*/
--- jstz_api/src/lib.rs
+++ jstz_api/src/lib.rs
@@ -2,6 +2,7 @@ pub(crate) mod console;
 pub(crate) mod error;
 pub(crate) mod ledger;
 pub(crate) mod conversion;
+pub(crate) mod storage;
 
 pub use console::ConsoleApi;
 pub use ledger::LedgerApi;
