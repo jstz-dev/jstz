a76705cdf5bfd5baa3523a0b1c82c0e59004df31
Sam Finch <sam.finch@trili.tech>
2023-09-25 14:42:59 +0100

    add minimal TextEncoder Api

---
+++ examples/encoding.js
@@ -0,0 +1,24 @@
+function encodeDecode(str) {
+  console.log(`encoding "${str}"`);
+  let b64 = TextEncoder.btoa(str);
+  console.info(b64);
+  console.log(`decoding "${b64}"`);
+  let back = TextEncoder.atob(b64);
+  console.info(back);
+}
+
+export default () => {
+  let test_string;
+  let test_strings =
+    ["hello world"
+     , JSON.stringify({foo: "bar"})
+     , "👋 from JSꜩ 🎉"
+    ];
+  try {
+    test_strings.forEach((str) => { test_string = str; encodeDecode(str)});
+  } catch (error){
+    console.error(`error decoding ${test_string}: ${error}`);
+    throw error;
+  }
+  return new Response("Success!")
+}
