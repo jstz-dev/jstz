fc89fc1564bfec68687ecc9b86194ed140e19f92
Alistair <ajo41@cam.ac.uk>
2023-10-03 12:53:33 +0100

    Format unformatted files

--- jstz_cli/src/deploy.rs
+++ jstz_cli/src/deploy.rs
@@ -22,108 +22,110 @@ pub fn deploy(script_address: String, cfg: &Config) {
 
     // Originate ctez contract
     let bootstrap3_address = "tz1faswCTDciRzE4oJ9jn2Vm2dvjeyA9fUzU";
-    let init_ctez_storage = format!("(Pair \"{}\" {{ Elt \"{}\" 10000000000 }} )", bootstrap3_address, bootstrap3_address);
+    let init_ctez_storage = format!(
+        "(Pair \"{}\" {{ Elt \"{}\" 10000000000 }} )",
+        bootstrap3_address, bootstrap3_address
+    );
 
-    
-    let output = cfg.octez_client_command()
-        .args(
-            [
-                "originate",
-                "contract",
-                "jstz_ctez",
-                "transferring",
-                "0",
-                "from",
-                "bootstrap3",
-                "running",
-                &ctez_src,
-                "--init",
-                &init_ctez_storage,
-                "--burn-cap",
-                "999",
-                "--force"
-            ]
-        )
+    let output = cfg
+        .octez_client_command()
+        .args([
+            "originate",
+            "contract",
+            "jstz_ctez",
+            "transferring",
+            "0",
+            "from",
+            "bootstrap3",
+            "running",
+            &ctez_src,
+            "--init",
+            &init_ctez_storage,
+            "--burn-cap",
+            "999",
+            "--force",
+        ])
         .output();
 
     handle_output(&output);
 
-    let ctez_address = extract_address(&output.expect("Failed to originate ctez contract").stdout);
+    let ctez_address =
+        extract_address(&output.expect("Failed to originate ctez contract").stdout);
 
     // Originate bridge contract
     let init_storage = format!("(Pair \"{}\" None)", ctez_address);
 
-    let output = cfg.octez_client_command()
-        .args(
-            [
-                "originate",
-                "contract",
-                "jstz_bridge",
-                "transferring",
-                "0",
-                "from",
-                "bootstrap3",
-                "running",
-                &src,
-                "--init",
-                &init_storage,
-                "--burn-cap",
-                "999",
-                "--force"
-            ]
-        )
+    let output = cfg
+        .octez_client_command()
+        .args([
+            "originate",
+            "contract",
+            "jstz_bridge",
+            "transferring",
+            "0",
+            "from",
+            "bootstrap3",
+            "running",
+            &src,
+            "--init",
+            &init_storage,
+            "--burn-cap",
+            "999",
+            "--force",
+        ])
         .output();
 
     handle_output(&output);
 
-    let bridge_address = extract_address(&output.expect("Failed to originate bridge contract").stdout);
+    let bridge_address =
+        extract_address(&output.expect("Failed to originate bridge contract").stdout);
 
     // Set ticketer
     let set_ticketer_emsg = format!("{{ \"SetTicketer\": \"{}\" }}", bridge_address);
     let set_ticketer_emsg_hex = hex::encode(set_ticketer_emsg);
     let hex_string = format!("hex:[ \"{}\" ]", set_ticketer_emsg_hex);
 
-    let output = cfg.octez_client_command()
-        .args(
-            [
-                "send",
-                "smart",
-                "rollup",
-                "message",
-                &hex_string,
-                "from",
-                "bootstrap2"
-            ]
-        )
+    let output = cfg
+        .octez_client_command()
+        .args([
+            "send",
+            "smart",
+            "rollup",
+            "message",
+            &hex_string,
+            "from",
+            "bootstrap2",
+        ])
         .output();
-    
+
     handle_output(&output);
 
-    let script_address_formatted=format!("\"{}\"", script_address);
+    let script_address_formatted = format!("\"{}\"", script_address);
 
     // Set rollup address
-    let output = cfg.octez_client_command()
-        .args(
-            [
-                "transfer",
-                "0",
-                "from",
-                "bootstrap3",
-                "to",
-                "jstz_bridge",
-                "--entrypoint",
-                "set_rollup",
-                "--arg",
-                &script_address_formatted,
-                "--burn-cap",
-                "999"
-            ]
-        )
+    let output = cfg
+        .octez_client_command()
+        .args([
+            "transfer",
+            "0",
+            "from",
+            "bootstrap3",
+            "to",
+            "jstz_bridge",
+            "--entrypoint",
+            "set_rollup",
+            "--arg",
+            &script_address_formatted,
+            "--burn-cap",
+            "999",
+        ])
         .output();
 
     handle_output(&output);
 
-    println!("The `jstz_bridge` contract has successfully been originated and configured.");
+    println!(
+        "The `jstz_bridge` contract has successfully been originated and configured."
+    );
     println!("You may now run `octez-client transfer 0 from .. to jstz_bridge ..` to communicate with `jstz_rollup` via the L1 layer.");
     println!("To upgrade the bridge, run this command again after running `make build-bridge`.");
 }
