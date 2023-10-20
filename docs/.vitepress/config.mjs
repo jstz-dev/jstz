503991fe3ce8d4049717a18778d4efdcc8cd015c
Alan Marko <alan.marko@trili.tech>
2023-10-20 16:13:23 +0100

    Added to config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -39,6 +39,7 @@ export default defineConfig({
           { text: "Ledger", link: "/api/ledger" },
           { text: "Headers", link: "/api/headers" },
           { text: "Response", link: "/api/response" },
+          { text: "URLSearchParams", link: "/api/url_search_params" },
         ],
       },
     ],
