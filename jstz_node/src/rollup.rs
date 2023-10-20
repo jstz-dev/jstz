5d76accbc87275c8dc43ecd867b14eaa563d7f06
Alistair <ajo41@cam.ac.uk>
2023-10-20 15:41:17 +0100

    Node: Create `jstz_node` and implement /operations/<hash>/receipt endpoint
    This commit creates the `jstz_node` binary: a HTTP server (using actix-web)
    with logging. Additionally it implements the GET /operations/<hash>/receipt
    endpoint (using `Rollup`) to read the receipt from the rollup node's durable
    storage.

---
+++ jstz_node/src/rollup.rs
@@ -0,0 +1,61 @@
+use anyhow::{anyhow, Result};
+use serde::Deserialize;
+
+#[derive(Debug)]
+pub struct RollupClient {
+    endpoint: String,
+    client: reqwest::Client,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ValueError {
+    pub kind: String,
+    pub id: String,
+    pub block: Option<String>,
+    pub msg: Option<String>,
+}
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+pub enum ValueResponse {
+    Value(String),
+    Errors(Vec<ValueError>),
+}
+
+impl RollupClient {
+    pub fn new(endpoint: String) -> Self {
+        Self {
+            endpoint,
+            client: reqwest::Client::new(),
+        }
+    }
+
+    pub async fn get_value(&self, key: &str) -> Result<Option<Vec<u8>>> {
+        let res = self
+            .client
+            .get(format!(
+                "{}/global/block/head/durable/wasm_2_0_0/value?key={}",
+                self.endpoint, key
+            ))
+            .send()
+            .await?;
+
+        if res.status() == 200 || res.status() == 500 {
+            let content: Option<ValueResponse> = res.json().await?;
+            match content {
+                Some(ValueResponse::Value(value)) => {
+                    let payload = hex::decode(value)?;
+                    Ok(Some(payload))
+                }
+                Some(ValueResponse::Errors(errors)) => Err(anyhow!(
+                    "Failed to get value of key-value pair: {}. Errors: {:?}",
+                    key,
+                    errors
+                )),
+                None => Ok(None),
+            }
+        } else {
+            Err(anyhow!("Unhandled response status: {}", res.status()))
+        }
+    }
+}
---
+++ jstz_node/src/services/operations.rs
@@ -0,0 +1,29 @@
+use actix_web::{
+    get,
+    web::{Data, Path, ServiceConfig},
+    HttpResponse, Responder, Scope,
+};
+
+use crate::{rollup::RollupClient, Result};
+
+#[get("/{hash}/receipt")]
+async fn receipt(
+    rollup_client: Data<RollupClient>,
+    path: Path<String>,
+) -> Result<impl Responder> {
+    let key = format!("/jstz_receipt/{}", path.into_inner());
+
+    let value = rollup_client.get_value(&key).await?;
+
+    Ok(HttpResponse::Ok().json(value))
+}
+
+pub struct OperationsService;
+
+impl OperationsService {
+    pub fn configure(cfg: &mut ServiceConfig) {
+        let scope = Scope::new("/operations").service(receipt);
+
+        cfg.service(scope);
+    }
+}
