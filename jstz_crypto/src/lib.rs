bcca1b1d6ea9e1b6296d2fddddff5a6bdd72df86
Alistair <ajo41@cam.ac.uk>
2023-10-24 18:02:16 +0100

    Crypto: Move `keypair_from_passphrase` to `lib`

--- jstz_crypto/src/lib.rs
+++ jstz_crypto/src/lib.rs
@@ -6,4 +6,15 @@ pub mod public_key;
 pub mod public_key_hash;
 pub mod secret_key;
 pub mod signature;
-pub mod utils;
+
+use tezos_crypto_rs::bls;
+
+use crate::{hash::Blake2b, public_key::PublicKey, secret_key::SecretKey};
+
+pub fn keypair_from_passphrase(passphrase: &str) -> Result<(SecretKey, PublicKey)> {
+    let ikm = Blake2b::from(passphrase.as_bytes());
+
+    let (sk, pk) = bls::keypair_from_ikm(*ikm.as_array())?;
+
+    Ok((SecretKey::Bls(sk), PublicKey::Bls(pk)))
+}
