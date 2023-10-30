25d748972a3157860816106ba2264fedb9378930
Alan Marko <alan.marko@trili.tech>
2023-10-30 16:54:59 +0000

    Unused warnings fixed

--- jstz_cli/src/deploy.rs
+++ jstz_cli/src/deploy.rs
@@ -1,8 +1,5 @@
 use anyhow::{anyhow, Result};
-use jstz_proto::{
-    context::account::Nonce,
-    operation::{Content, DeployContract, Operation, SignedOperation},
-};
+use jstz_proto::operation::{Content, DeployContract, Operation, SignedOperation};
 
 use crate::{
     config::Config,
