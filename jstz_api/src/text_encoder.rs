84def0a75cba19169b9ec69161d27f8109813a3e
Remi Lesenechal <lesenechal.remi@gmail.com>
2023-11-07 16:16:32 +0100

    Version using the encoding crate

--- jstz_api/src/text_encoder.rs
+++ jstz_api/src/text_encoder.rs
@@ -9,6 +9,8 @@ use boa_engine::{
     NativeFunction,
 };
 use boa_gc::{Finalize, GcRefMut, Trace};
+use encoding::all::UTF_8;
+use encoding::{EncoderTrap, Encoding};
 use jstz_core::{
     accessor,
     native::{
@@ -98,8 +100,6 @@ impl NativeClass for TextEncoderEncodeIntoResult {
     }
 }
 
-type Uint8Array = Box<[u8]>;
-
 impl TextEncoder {
     fn try_from_js<'a>(value: &'a JsValue) -> JsResult<GcRefMut<'a, Object, Self>> {
         value
@@ -123,34 +123,69 @@ impl TextEncoder {
         )
     }
 
-    fn encode(input: Option<String>) -> Uint8Array {
-        let s = input.unwrap_or_else(|| "".to_string());
-        s.into_bytes().into_boxed_slice()
+    fn encode(input: Option<&[u16]>) -> Vec<u8> {
+        //  handle optional argument
+        let input = input.unwrap_or_else(|| &[]);
+        //  1. Convert input to an I/O queue of scalar values.
+        //  NB: here we just convert a u16 slice to a string slice using rust `String::from_utf16_lossy`
+        //      it seems `encode` reads unpaired surrogates (i.e. U+DC00) as replacement character U+FFFD
+        let input = String::from_utf16_lossy(input);
+        let input: &str = input.as_str();
+        //  2. Let output be the I/O queue of bytes << end-of-queue >>.
+        //  NB: In Rust, we don't require end-of-queue item
+
+        //  3. While true: ...
+        //  NB: this is performed by the rust `encoding` crate. The "fatal" ErrorMode corresponds to `EncoderTrap::Strict`
+        UTF_8.encode(input, EncoderTrap::Strict).unwrap()
+    }
+
+    fn custom_decode_utf16(input: &[u16]) -> String {
+        let input = input.into_iter().map(|&codepoint| codepoint);
+        let mut output: Vec<char> = Vec::new();
+        let mut utf16_decoder = char::decode_utf16(input);
+        while let Some(result) = utf16_decoder.next() {
+            match result {
+                Ok(s) => output.push(s),
+                Err(_) => break,
+            }
+        }
+        output.into_iter().collect()
     }
 
     fn encode_into(
-        source: String,
-        destination: &mut Uint8Array,
+        source: &[u16],
+        destination: &mut Vec<u8>,
     ) -> TextEncoderEncodeIntoResult {
-        let source = source.into_bytes().into_boxed_slice();
-        let mut count = 0u128;
-        for (i, dst_elt) in destination.iter_mut().enumerate() {
-            if let Some(src_elt) = source.get(i) {
-                count += 1;
-                *dst_elt = *src_elt;
-            }
-        }
-        TextEncoderEncodeIntoResult {
-            read: count,
-            written: count,
-        }
+        //  1. Let read be 0
+        //  2. Let written be 0.
+        //  NB: read and written both correspond to the same value as UTF encoder can't fail
+        //      they are later set to `source` length
+        let read = source.len() as u128;
+        let written = read;
+
+        //  3. Let encoder be an instance of the UTF-8 encoder
+        //  NB: Noop, we only support UTF-8 encoder
+        //  4. Let unused be the I/O queue of scalar values << end-of-queue >>.
+        //  NB: The handler algorithm invoked below requires this argument, but it is not used by the UTF-8 encoder.
+        //  Therefore Noop
+        //  5. Convert src to an I/O queue of scalar values.
+        //  NB: here we just convert a u16 slice to a string slice using our custom decoder
+        //      it will stop after the first unpaired surrogate
+        let source = Self::custom_decode_utf16(source);
+        let input = source.as_str();
+        //  6. While true: ...
+        //  NB: this is performed by the rust `encoding` crate.
+        let () = UTF_8
+            .encode_to(input, EncoderTrap::Call(|_, _, _| false), destination)
+            .unwrap();
+        TextEncoderEncodeIntoResult { read, written }
     }
 }
 #[derive(Default, Clone, Trace, Finalize)]
 pub struct TextEncoderClass;
 impl TextEncoderClass {
     fn encode(_: &JsValue, args: &[JsValue], context: &mut Context) -> JsResult<JsValue> {
-        let arg = args.get_or_undefined(0).try_js_into(context)?;
+        let arg = args.get_or_undefined(0).as_string().map(|x| x.as_slice());
         let result = TextEncoder::encode(arg);
         let byte_block = result.to_vec();
         let array_buffer: JsArrayBuffer =
@@ -166,22 +201,19 @@ impl TextEncoderClass {
     ) -> JsResult<JsValue> {
         let source = args
             .get_or_undefined(0)
-            .try_js_into(context)
-            .map_or(String::from(""), |x| x);
+            .as_string()
+            .map(|x| x.as_slice())
+            .unwrap();
         let destination = args.get_or_undefined(1);
         // 2nd argument is a reference to a Uint8Array to be overwritten
         let init: JsUint8Array = destination.try_js_into(context)?;
         // initialize a boxed "destination buffer" with the transpiled content of the Js object
-        let mut vec: Vec<u8> = Vec::with_capacity(init.length(context)?);
-        for index in 0..init.length(context)? {
-            vec.push(init.at(index as i64, context)?.to_uint8(context)?)
-        }
-        let destination_buffer: &mut Box<[u8]> = &mut Box::from(vec);
+        let destination: &mut Vec<u8> = &mut Vec::new();
         // call the rust function
         let result: TextEncoderEncodeIntoResult =
-            TextEncoder::encode_into(source, destination_buffer);
+            TextEncoder::encode_into(source, destination);
         // write back the destination JsValue
-        for (i, value) in destination_buffer.iter().enumerate() {
+        for (i, value) in destination.iter().enumerate() {
             init.fill(*value, Some(i), Some(i + 1), context)?;
         }
         // create and return a TextEncoderEncodeIntoResult object
@@ -264,4 +296,9 @@ const encoded = encoder.encode(text);
 
 encoded instanceof Uint8Array == true;
 encoded.length == text.length;
+*/
+
+/*
+const encoder = new TextEncoder();
+encoder.encode("\udc00");
 */
