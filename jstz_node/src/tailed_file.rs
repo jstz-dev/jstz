3713f0c2c9b78d4105e8a80bb1e4fb23aefaf15c
Leo <leo.unoki@trili.tech>
2023-11-14 13:44:58 +0000

    Implement TailedFile

---
+++ jstz_node/src/tailed_file.rs
@@ -0,0 +1,21 @@
+use std::io::SeekFrom;
+use tokio::io::{AsyncSeekExt, BufReader, Result};
+use tokio::{fs::File, io::Lines};
+
+pub struct TailedFile(BufReader<File>);
+
+pub use tokio::io::AsyncBufReadExt;
+
+impl TailedFile {
+    pub async fn init(path: &str) -> Result<Self> {
+        let mut file = File::open(path).await?;
+        let _ = file.seek(SeekFrom::End(0));
+        let reader = BufReader::new(file);
+
+        return Ok(TailedFile(reader));
+    }
+
+    pub fn lines(self) -> Lines<BufReader<File>> {
+        self.0.lines()
+    }
+}
