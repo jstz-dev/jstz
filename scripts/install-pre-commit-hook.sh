eee53ab71db2f821ef49ddb6dc2faa6a6cb6956d
Alistair <ajo41@cam.ac.uk>
2023-12-04 12:12:00 +0000

    style(commits): add commit-msg hook for commitlint
    This commit additionally updates our `install-hooks` script to install
    the commit-msg hook

--- scripts/install-pre-commit-hook.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cp ./scripts/pre-commit-hook.sh .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
-echo "Successfully install pre-commit hook (scripts/pre-commit-hook.sh) into .git/hooks."
