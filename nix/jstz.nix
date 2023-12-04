9ee547566b68ba15477904e9be1338bbaaf3e881
Ole Krüger <ole.kruger@trili.tech>
2023-12-04 17:00:20 +0000

    Fix the Kernel SDK branch to master

--- nix/jstz.nix
+++ nix/jstz.nix
@@ -42,7 +42,7 @@
     cargoLock = {
       lockFile = ../Cargo.lock;
       outputHashes = {
-        "tezos-smart-rollup-0.2.2" = "sha256-2wJFG8IvbTyBa06rvNjN1OrOQSmDQXT2G1gsAXMDe4Q=";
+        "tezos-smart-rollup-0.2.2" = "sha256-mhgIqPh7fh7cANBrJFDE8hxvgKXNOF2ZvLgDunTUoCk=";
         "boa_engine-0.17.0" = "sha256-bf6i5ESIHwepb1a4dUYREPprz7Rijq+P5z+NXpsT16Q=";
       };
     };
