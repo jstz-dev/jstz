d0b20dc76fbf566decb1128143b9a31893626535
Ole Krüger <ole.kruger@trili.tech>
2023-12-05 10:09:15 +0000

    build(nix): fix Darwin CC flags dilemma when targeting WASM

--- flake.nix
+++ flake.nix
@@ -32,6 +32,23 @@
               frameworks
             );
 
+          clangNoArch =
+            if pkgs.stdenv.isDarwin
+            then
+              pkgs.clang.overrideAttrs (old: {
+                postFixup = ''
+                  ${old.postFixup or ""}
+
+                  # On macOS this contains '-march' and '-mcpu' flags. These flags
+                  # would be used for any invocation of Clang.
+                  # Removing those makes the resulting Clang wrapper usable when
+                  # cross-compiling where passing '-march' and '-mcpu' would not
+                  # make sense.
+                  echo > $out/nix-support/cc-cflags-before
+                '';
+              })
+            else pkgs.clang;
+
           jstz = pkgs.callPackage ./nix/jstz.nix {makeFrameworkFlags = makeFrameworkFlags;};
         in {
           packages = {
@@ -40,8 +57,7 @@
           };
 
           # Rust dev environment
-          devShells.default = pkgs.mkShell rec {
-            NIX_CFLAGS_COMPILE = "-mcpu=generic";
+          devShells.default = pkgs.mkShell {
             NIX_LDFLAGS = pkgs.lib.optional pkgs.stdenv.isDarwin (
               makeFrameworkFlags [
                 "Security"
@@ -50,6 +66,12 @@
             );
 
             CC = "clang";
+
+            # This tells the 'cc' Rust crate to build using this C compiler when
+            # targeting other architectures.
+            CC_wasm32_unknown_unknown = "${clangNoArch}/bin/clang";
+            CC_riscv64gc_unknown_hermit = "${clangNoArch}/bin/clang";
+
             hardeningDisable =
               pkgs.lib.optionals
               (pkgs.stdenv.isAarch64 && pkgs.stdenv.isDarwin)
