6e1128965e818479a07fcda38397909f2c392e7b
Leo <leo.unoki@trili.tech>
2023-12-08 16:55:41 +0000

    feat(cli): support alias for logs trace <sf>

--- crates/jstz_cli/src/logs/mod.rs
+++ crates/jstz_cli/src/logs/mod.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Result};
 use clap::Subcommand;
 use jstz_crypto::public_key_hash::PublicKeyHash;
+use jstz_proto::context::account::Address;
 
 use crate::config::Config;
 
@@ -10,17 +11,29 @@ mod trace;
 pub enum Command {
     /// View logs
     Trace {
-        #[arg(value_name = "SMART_FUNCTIOIN")]
-        address: String,
+        // The address or the alias of the smart function
+        #[arg(value_name = "ALIAS|ADDRESS")]
+        smart_function: String,
     },
 }
 
 pub async fn exec(command: Command, cfg: &mut Config) -> Result<()> {
     match command {
-        Command::Trace { address } => {
-            let address = PublicKeyHash::from_base58(&address)
-                .map_err(|_| anyhow!("Invalid smart function address"))?;
+        Command::Trace { smart_function } => {
+            let address = get_address_for_smart_function(&smart_function, cfg)?;
             trace::exec(address, cfg).await
         }
     }
+}
+
+fn get_address_for_smart_function(smart_function: &str, cfg: &Config) -> Result<Address> {
+    if let Ok(address) = PublicKeyHash::from_base58(smart_function) {
+        return Ok(address);
+    }
+
+    if let Ok(account) = cfg.accounts.get(smart_function) {
+        return Ok(account.address().clone());
+    }
+
+    Err(anyhow!("Invalid smart function alias or address"))
 }
