1fbe2b3b5e6461ca82e726b338ad2263b896ef91
Leo <leo.unoki@trili.tech>
2023-12-11 13:48:33 +0000

    refactor(cli): move get_address to config.rs

--- crates/jstz_cli/src/logs/mod.rs
+++ crates/jstz_cli/src/logs/mod.rs
@@ -1,7 +1,5 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use clap::Subcommand;
-use jstz_crypto::public_key_hash::PublicKeyHash;
-use jstz_proto::context::account::Address;
 
 use crate::config::Config;
 
@@ -19,21 +17,6 @@ pub enum Command {
 
 pub async fn exec(command: Command, cfg: &mut Config) -> Result<()> {
     match command {
-        Command::Trace { smart_function } => {
-            let address = get_address_for_smart_function(&smart_function, cfg)?;
-            trace::exec(address, cfg).await
-        }
+        Command::Trace { smart_function } => trace::exec(smart_function, cfg).await,
     }
-}
-
-fn get_address_for_smart_function(smart_function: &str, cfg: &Config) -> Result<Address> {
-    if let Ok(address) = PublicKeyHash::from_base58(smart_function) {
-        return Ok(address);
-    }
-
-    if let Ok(account) = cfg.accounts.get(smart_function) {
-        return Ok(account.address().clone());
-    }
-
-    Err(anyhow!("Invalid smart function alias or address"))
 }
