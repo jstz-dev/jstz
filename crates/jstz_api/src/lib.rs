7f4fc6fb5c2c3aa7eb727b8c9076d2991edc2e1a
Leo <leo.unoki@trili.tech>
2023-12-11 17:02:17 +0000

    feat(api): use logger trait for logging

--- crates/jstz_api/src/lib.rs
+++ crates/jstz_api/src/lib.rs
@@ -7,7 +7,7 @@ pub mod idl;
 pub mod random;
 pub mod url;
 pub mod urlpattern;
-pub use console::{ConsoleApi, LogRecord, LOG_PREFIX};
+pub use console::{set_js_logger, ConsoleApi, JsLog, LogData, LogLevel};
 pub use kv::Kv;
 pub use kv::KvApi;
 pub use kv::KvValue;
