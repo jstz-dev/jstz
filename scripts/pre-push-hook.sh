1ffde418e9f9e72b890c24bfc913c60293b1a540
Leo <leo.unoki@trili.tech>
2023-12-13 10:02:51 +0000

    ci: add pre-push-hook

---
+++ scripts/pre-push-hook.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+# Run linting
+make lint
+
+# Check if linting passed
+if [ $? -ne 0 ]; then
+  echo "Linting failed. Aborting push."
+  exit 1
+fi
+
+exit 0
