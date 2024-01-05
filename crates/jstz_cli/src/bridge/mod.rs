5be3bd55c1b9d40220bf24050981ad9ca7f2a88d
Alistair <ajo41@cam.ac.uk>
2024-01-05 16:38:50 +0000

    feat(cli): update jstz to use `jstz_rollup` crate

--- crates/jstz_cli/src/bridge/mod.rs
+++ crates/jstz_cli/src/bridge/mod.rs
@@ -5,52 +5,6 @@ mod deposit;
 
 use crate::config::Config;
 
-const BOOTSTRAP3_ADDRESS: &str = "tz1faswCTDciRzE4oJ9jn2Vm2dvjeyA9fUzU";
-
-pub fn deploy(cfg: &Config) -> Result<String> {
-    let bridge_dir = cfg.jstz_path.join("contracts");
-
-    // 1. Originate ctez contract
-    let init_ctez_storage = format!(
-        "(Pair \"{}\" {{ Elt \"{}\" 10000000000 }} )",
-        BOOTSTRAP3_ADDRESS, BOOTSTRAP3_ADDRESS
-    );
-
-    let ctez_address = cfg.octez_client()?.originate_contract(
-        "jstz_ctez",
-        "bootstrap3",
-        bridge_dir
-            .join("jstz_ctez.tz")
-            .to_str()
-            .expect("Invalid path"),
-        &init_ctez_storage,
-    )?;
-
-    // 2. Originate bridge contract
-    let init_bridge_storage = format!("(Pair \"{}\" None)", ctez_address);
-
-    let bridge_address = cfg.octez_client()?.originate_contract(
-        "jstz_bridge",
-        "bootstrap3",
-        bridge_dir
-            .join("jstz_bridge.tz")
-            .to_str()
-            .expect("Invalid path"),
-        &init_bridge_storage,
-    )?;
-
-    Ok(bridge_address)
-}
-
-pub fn set_rollup(cfg: &Config, rollup_address: &str) -> Result<()> {
-    cfg.octez_client()?.call_contract(
-        "bootstrap3",
-        "jstz_bridge",
-        "set_rollup",
-        &format!("\"{}\"", rollup_address),
-    )
-}
-
 #[derive(Subcommand)]
 pub enum Command {
     /// Deposits from an existing L1 sandbox address to a L2 sandbox address.
--- crates/jstz_cli/src/sandbox/mod.rs
+++ crates/jstz_cli/src/sandbox/mod.rs
@@ -7,6 +7,8 @@ use nix::{
 
 mod daemon;
 
+pub(crate) use daemon::CLIENT_ADDRESS;
+
 use crate::config::Config;
 
 #[derive(Subcommand)]
