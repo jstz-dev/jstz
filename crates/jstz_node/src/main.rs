596ceec088aff2086c2d17afba7b7bae3a81e1f9
Alan Marko <alan.marko@trili.tech>
2024-01-26 18:05:43 +0000

    feat(node): separate node runner
    feat(node): moving node to sandbox

--- crates/jstz_node/src/main.rs
+++ crates/jstz_node/src/main.rs
@@ -4,22 +4,26 @@ use crate::services::{AccountsService, OperationsService, Service};
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use clap::Parser;
 use env_logger::Env;
+use jstz_cli::sandbox::{
+    DEFAULT_ROLLUP_NODE_RPC_ADDR, DEFAULT_ROLLUP_RPC_PORT, ENDPOINT,
+};
+use octez::OctezRollupClient;
+use services::{LogsService, Service};
+use tokio_util::sync::CancellationToken;
+
+use crate::{
+    node_runner::run_node,
+    services::{AccountsService, OperationsService},
+};
 pub use error::{Error, Result};
 use octez::OctezRollupClient;
 use services::LogsService;
 use tokio_util::sync::CancellationToken;
 
 mod error;
+mod node_runner;
 mod services;
 mod tailed_file;
-
-/// Endpoint defaults for the `octez-smart-rollup-node`
-const DEFAULT_ROLLUP_NODE_RPC_ADDR: &str = "127.0.0.1";
-const DEFAULT_ROLLUP_RPC_PORT: u16 = 8932;
-
-/// Endpoint defaults for the `jstz-node`
-const DEFAULT_JSTZ_NODE_ADDR: &str = "127.0.0.1";
-const DEFAULT_JSTZ_NODE_PORT: u16 = 8933;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -45,40 +49,11 @@ struct Args {
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     let args = Args::parse();
-
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
-
-    let rollup_endpoint = args.rollup_endpoint.unwrap_or(format!(
-        "http://{}:{}",
-        args.rollup_node_rpc_addr, args.rollup_node_rpc_port
-    ));
-
-    let rollup_client = Data::new(OctezRollupClient::new(rollup_endpoint));
-
-    let cancellation_token = CancellationToken::new();
-
-    let (broadcaster, db, tail_file_handle) =
-        LogsService::init(args.kernel_file_path, &cancellation_token)
-            .await
-            .map_err(|e| io::Error::new(Other, e.to_string()))?;
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(rollup_client.clone())
-            .configure(OperationsService::configure)
-            .configure(AccountsService::configure)
-            .app_data(Data::from(broadcaster.clone()))
-            .app_data(Data::new(db.clone()))
-            .configure(LogsService::configure)
-            .wrap(Logger::default())
-    })
-    .bind((args.addr, args.port))?
-    .run()
-    .await?;
-
-    cancellation_token.cancel();
-
-    tail_file_handle.await.unwrap()?;
-
-    Ok(())
+    run_node(
+        args.rollup_node_rpc_addr,
+        args.rollup_node_rpc_port,
+        args.rollup_endpoint,
+        args.kernel_file_path,
+    )
+    .await
 }
