cf2f0eb3f27399ff3a4b31d3162fd9be69e64218
ryutamago <leo.unoki@trili.tech>
2024-02-21 10:42:30 +0000

    refactor(cli): implement exec_trace

--- crates/jstz_cli/src/logs/trace.rs
+++ crates/jstz_cli/src/logs/trace.rs
@@ -1,8 +1,8 @@
-use futures_util::stream::StreamExt;
+use futures_util::{stream::StreamExt, Future};
 use jstz_api::js_log::LogLevel;
 use jstz_proto::js_logger::LogRecord;
 use log::{debug, error, info};
-use reqwest_eventsource::Event;
+use reqwest_eventsource::{Event, EventSource};
 
 use crate::{config::NetworkName, error::Result, utils::AddressOrAlias, Config};
 
@@ -18,11 +18,30 @@ pub async fn exec(
     let address = address_or_alias.resolve(&cfg)?;
     debug!("resolved `address_or_alias` -> {:?}", address);
 
-    let mut event_source = cfg.jstz_client(network)?.logs_stream(&address);
+    let event_source = cfg.jstz_client(network)?.logs_stream(&address);
 
+    exec_trace(event_source, log_level, || async {
+        info!("Connected to smart function '{}'.", address);
+    })
+    .await?;
+
+    Ok(())
+}
+
+pub async fn exec_trace<F, Fut>(
+    mut event_source: EventSource,
+    log_level: LogLevel,
+    on_connect: F,
+) -> Result<()>
+where
+    F: Fn() -> Fut,
+    Fut: Future<Output = ()>,
+{
     while let Some(event) = event_source.next().await {
         match event {
-            Ok(Event::Open) => info!("Event source opened."),
+            Ok(Event::Open) => {
+                on_connect().await;
+            }
             Ok(Event::Message(message)) => {
                 if let Ok(log_record) = serde_json::from_str::<LogRecord>(&message.data) {
                     let LogRecord { level, text, .. } = log_record;
