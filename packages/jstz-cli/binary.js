ba9f26b95687b3e831098c51891a159a0767a44c
Alistair <ajo41@cam.ac.uk>
2024-05-23 16:16:08 +0100

    feat(jstz): add support for installing jstz via npm

---
+++ packages/jstz-cli/binary.js
@@ -0,0 +1,62 @@
+const { Binary } = require("binary-install");
+const { table } = require("table");
+const os = require("os");
+const { join } = require("path");
+
+const { version, name, repository } = require("./package.json");
+
+const SUPPORTED_PLATFORMS = [
+  { os: "darwin", arch: "x64", target: "x86_64-unknown-linux-musl" },
+  // FIXME: For now we rely on Rosetta 2 to run the x86_64 binary on M1 Macs
+  { os: "darwin", arch: "arm64", target: "x86_64-apple-darwin" },
+  { os: "linux", arch: "x64", target: "x86_64-apple-darwin" },
+];
+
+const error = (msg) => {
+  console.error(msg);
+  process.exit(1);
+};
+
+const supportedPlatformsTable = () =>
+  table(
+    SUPPORTED_PLATFORMS.map(({ os, arch }) => [os, arch]).unshift([
+      "OS",
+      "Arch",
+    ]),
+  );
+
+const getPlatform = () => {
+  const type = os.type();
+  const arch = os.arch();
+
+  for (const platform of SUPPORTED_PLATFORMS) {
+    if (platform.os === type && platform.arch === arch) {
+      return platform;
+    }
+  }
+
+  error(
+    `Platform ${type}-${arch} is not supported.\nYour platform must be one of the following:\n\n${supportedPlatformsTable()}`,
+  );
+};
+
+const getBinary = () => {
+  const platform = getPlatform();
+  const url = `${repository.url}/releases/download/v${version}/${name}-${platform.os}-${platform.target}.tar.gz`;
+
+  return new Binary(platformMetadata.BINARY_NAME, url, version, {
+    installDirectory: join(__dirname, "node_modules", ".bin"),
+  });
+};
+
+const run = () => {
+  const binary = getBinary();
+  binary.run();
+};
+
+const install = () => {
+  const binary = getBinary();
+  binary.install();
+};
+
+module.exports = { install, run };
