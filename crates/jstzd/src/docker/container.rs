7d0e1262b381114685ecda4eb0ea0889564cd6a8
ryutamago <leo.unoki@trili.tech>
2024-09-18 15:02:15 +0100

    feat(jstzd): implement container

---
+++ crates/jstzd/src/docker/container.rs
@@ -0,0 +1,62 @@
+use anyhow::Result;
+use bollard::Docker;
+use std::sync::Arc;
+
+pub struct Container {
+    pub id: String,
+    client: Option<Arc<Docker>>,
+    _private: (),
+}
+
+impl Container {
+    /// Creates a new container with running `id`
+    pub(super) fn new(client: Arc<Docker>, id: String) -> Self {
+        Self {
+            id,
+            client: Some(client),
+            _private: (),
+        }
+    }
+
+    // Starts the container's entrypoint
+    pub async fn start(&self) -> Result<()> {
+        self.client()?
+            .start_container::<String>(&self.id, None)
+            .await?;
+        Ok(())
+    }
+
+    // Stop the container
+    pub async fn stop(&self) -> Result<()> {
+        self.client()?.stop_container(&self.id, None).await?;
+        Ok(())
+    }
+
+    // Remove the container
+    pub async fn remove(&self) -> Result<()> {
+        match self.client()?.remove_container(&self.id, None).await {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                if e.to_string().contains("404") {
+                    return Err(anyhow::anyhow!(
+                        "Failed to remove non existent container: {}",
+                        self.id
+                    ));
+                }
+                return Err(e.into());
+            }
+        }
+    }
+
+    // Stop and remove the container, should be called when dropping the container
+    pub async fn cleanup(&self) -> Result<()> {
+        self.stop().await?;
+        self.remove().await
+    }
+
+    fn client(&self) -> Result<&Arc<Docker>> {
+        self.client
+            .as_ref()
+            .ok_or_else(|| anyhow::anyhow!("Client does not exist"))
+    }
+}
--- crates/jstzd/src/docker/runnable_image.rs
+++ crates/jstzd/src/docker/runnable_image.rs
@@ -1,5 +1,18 @@
+use bollard::{
+    container::{Config, CreateContainerOptions},
+    secret::{ContainerCreateResponse, HostConfig, PortBinding, PortMap},
+    Docker,
+};
+
 use crate::docker::Image;
-use std::{collections::BTreeMap, net::IpAddr};
+use anyhow::Result;
+use std::{
+    collections::{BTreeMap, HashMap, HashSet},
+    net::IpAddr,
+    sync::Arc,
+};
+
+use super::Container;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum AccessMode {
@@ -101,6 +114,105 @@ impl<I: Image> RunnableImage<I> {
         self.ports.insert(host_port, container_port);
         self
     }
+
+    pub async fn create_container(
+        self,
+        client: Arc<Docker>,
+    ) -> anyhow::Result<Container> {
+        self.all_container_ports_are_exposed()?;
+        let config = Config::<String> {
+            image: Some(self.image.image_uri().to_string()),
+            host_config: Some(self.host_config()),
+            entrypoint: self.entrypoint(),
+            cmd: self.overridden_cmd.clone(),
+            env: self.env(),
+            ..Config::default()
+        };
+        let options = Some(CreateContainerOptions {
+            name: self.container_name.clone(),
+            platform: None,
+        });
+        match client.create_container(options, config).await {
+            Ok(ContainerCreateResponse { id, .. }) => Ok(Container::new(client, id)),
+            Err(err) => {
+                let err_msg = err.to_string();
+                if err_msg.contains("404") {
+                    return Err(anyhow::anyhow!(
+                        "Image: {} not found, please make sure the image is pulled",
+                        self.image.image_uri()
+                    ));
+                }
+                return Err(err.into());
+            }
+        }
+    }
+
+    fn env(&self) -> Option<Vec<String>> {
+        match self.env_vars.is_empty() {
+            true => None,
+            false => Some(
+                self.env_vars
+                    .iter()
+                    .map(|(k, v)| format!("{}={}", k, v))
+                    .collect::<Vec<_>>(),
+            ),
+        }
+    }
+
+    fn entrypoint(&self) -> Option<Vec<String>> {
+        self.image.entrypoint().map(|e| {
+            e.split_whitespace()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
+        })
+    }
+
+    fn host_config(&self) -> HostConfig {
+        let port_bindings: PortMap = self.ports.iter().fold(
+            HashMap::new(),
+            |mut acc, (host_port, container_port)| {
+                let container_port = container_port.to_string();
+                let binding = PortBinding {
+                    host_ip: None,
+                    host_port: Some(host_port.to_string()),
+                };
+                acc.entry(container_port)
+                    .and_modify(|bindings| {
+                        if let Some(vec) = bindings {
+                            vec.push(binding.clone());
+                        }
+                    })
+                    .or_insert(Some(vec![binding]));
+                acc
+            },
+        );
+        let extra_hosts = self
+            .hosts
+            .iter()
+            .map(|(host, ip)| format!("{}:{}", host, ip.to_string()))
+            .collect::<Vec<_>>();
+        let mut config = HostConfig::default();
+        if !port_bindings.is_empty() {
+            config.port_bindings = Some(port_bindings);
+        }
+        if !extra_hosts.is_empty() {
+            config.extra_hosts = Some(extra_hosts);
+        }
+        config
+    }
+    // Check if the ports use the exposed container ports from the image if any
+    fn all_container_ports_are_exposed(&self) -> Result<()> {
+        let exposed_ports: HashSet<&u16> = self.image.exposed_ports().iter().collect();
+        if exposed_ports.is_empty() {
+            return Ok(());
+        }
+        let all_ports_are_exposed =
+            self.ports.values().all(|port| exposed_ports.contains(port));
+        if !all_ports_are_exposed {
+            return Err(anyhow::anyhow!("Invalid ports"));
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -154,5 +266,73 @@ mod test {
             runnable_image.ports,
             vec![(5000, 3000)].into_iter().collect()
         );
+    }
+
+    #[test]
+    fn parses_env() {
+        let image = GenericImage::new("busybox");
+        let runnable_image =
+            RunnableImage::new(image, "busybox_test").with_env_var("KEY", "VALUE");
+        let env: Option<Vec<String>> = runnable_image.env();
+        assert_eq!(env, Some(vec!["KEY=VALUE".to_string()]));
+    }
+
+    #[test]
+    fn parses_entrypoint() {
+        let image = GenericImage::new("busybox").with_entrypoint("echo hello");
+        let runnable_image = RunnableImage::new(image, "busybox_test");
+        let entrypoint: Option<Vec<String>> = runnable_image.entrypoint();
+        assert_eq!(
+            entrypoint,
+            Some(vec!["echo".to_string(), "hello".to_string()])
+        );
+    }
+
+    #[test]
+    fn parses_host_config() {
+        let image = GenericImage::new("busybox");
+        let runnable_image = RunnableImage::new(image, "busybox_test")
+            .with_host("hostname", Host::HostGateway)
+            .with_port(4000, 3000)
+            .with_port(5000, 3000);
+        let host_config = runnable_image.host_config();
+        assert_eq!(
+            host_config.extra_hosts,
+            Some(vec!["hostname:host-gateway".to_string()])
+        );
+
+        let port_bindings = host_config.port_bindings.expect("port bindings not found");
+        assert_eq!(
+            port_bindings.get("3000").unwrap(),
+            &Some(vec![
+                PortBinding {
+                    host_ip: None,
+                    host_port: Some("4000".to_string())
+                },
+                PortBinding {
+                    host_ip: None,
+                    host_port: Some("5000".to_string())
+                }
+            ])
+        );
+    }
+
+    #[test]
+    fn throws_invalid_ports() {
+        let image = GenericImage::new("busybox").with_exposed_ports(&[3000]);
+        let runnable_image =
+            RunnableImage::new(image, "busybox_test").with_port(3001, 3001);
+        let result = runnable_image.all_container_ports_are_exposed();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn validates_ports() {
+        let image = GenericImage::new("busybox").with_exposed_ports(&[3000, 4000]);
+        let runnable_image = RunnableImage::new(image, "busybox_test")
+            .with_port(3000, 3000)
+            .with_port(3000, 4000);
+        let result = runnable_image.all_container_ports_are_exposed();
+        assert!(result.is_ok());
     }
 }
