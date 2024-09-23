3587d4ea4eaf9e7a9402dbdf43fe98e6dbd99580
Alistair <ajo41@cam.ac.uk>
2024-09-23 13:52:27 +0100

    fix: wip

--- crates/jstzd/tests/dummy.rs
+++ crates/jstzd/tests/dummy.rs
@@ -1,9 +1,14 @@
-use bollard::Docker;
+use bollard::{Docker, API_DEFAULT_VERSION};
 use jstzd::main;
 
 #[tokio::test]
 async fn test_main() {
-    let docker = Docker::connect_with_unix_defaults().unwrap();
+    let docker = Docker::connect_with_unix(
+        "unix:///var/run/docker.sock",
+        120,
+        API_DEFAULT_VERSION,
+    )
+    .unwrap();
     let version = docker.version().await.unwrap();
     println!("{:?}", version);
 
