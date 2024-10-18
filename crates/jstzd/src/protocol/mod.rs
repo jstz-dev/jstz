2573d2b4d1f82d01d5d77c971c758ec078a42de9
Huan-Cheng Chang <huancheng.chang@trili.tech>
2024-10-18 17:56:18 +0100

    feat(jstzd): add bootstrap accounts to ProtocolParameterBuilder

--- crates/jstzd/src/protocol/mod.rs
+++ crates/jstzd/src/protocol/mod.rs
@@ -1,10 +1,14 @@
+mod bootstrap;
+
+pub use bootstrap::BootstrapAccount;
+use bootstrap::BootstrapAccounts;
 use rust_embed::Embed;
 use serde_json::Value;
 use std::fmt::Display;
 use std::io::{Read, Seek, Write};
 use std::path::{Path, PathBuf};
 
-pub trait ReadWritable: Read + std::io::Write {}
+pub trait ReadWritable: Read + Write {}
 
 impl ReadWritable for tempfile::NamedTempFile {}
 
@@ -60,6 +64,8 @@ pub struct ProtocolParameterBuilder {
     protocol: Option<Protocol>,
     /// Protocol constants.
     constants: Option<ProtocolConstants>,
+    /// Bootstrap accounts.
+    bootstrap_accounts: BootstrapAccounts,
     /// Path to an existing parameter file whose content will be used as the base
     /// parameter set. Optional. If `source_path` is not given, a predefined parameter
     /// file will be used instead depending on `protocol` and `constants`.
@@ -81,6 +87,16 @@ impl ProtocolParameterBuilder {
         self
     }
 
+    pub fn set_bootstrap_accounts(
+        &mut self,
+        accounts: Vec<BootstrapAccount>,
+    ) -> &mut Self {
+        for account in accounts {
+            self.bootstrap_accounts.add_account(account);
+        }
+        self
+    }
+
     pub fn set_source_path(&mut self, path: &str) -> &mut Self {
         self.source_path = Some(PathBuf::from(path));
         self
@@ -94,6 +110,8 @@ impl ProtocolParameterBuilder {
         let json = raw_json.as_object_mut().ok_or(anyhow::anyhow!(
             "Failed to convert loaded json file into a json object"
         ))?;
+
+        self.inject_bootstrap_contracts(json)?;
 
         let mut output_file = tempfile::NamedTempFile::new().unwrap();
         serde_json::to_writer(output_file.as_file(), &json)?;
@@ -142,11 +160,67 @@ impl ProtocolParameterBuilder {
         };
         Ok(raw_json)
     }
+
+    fn inject_bootstrap_contracts(
+        &mut self,
+        json: &mut serde_json::Map<String, Value>,
+    ) -> anyhow::Result<()> {
+        let mut accounts = BootstrapAccounts::default();
+        if let Some(value) = json.get("bootstrap_accounts") {
+            if let Some(existing_accounts) = value.as_array() {
+                for account in existing_accounts {
+                    accounts.add_account(BootstrapAccount::try_from(account)?);
+                }
+            }
+        }
+        for account in self.bootstrap_accounts.get_accounts() {
+            accounts.add_account(account.clone());
+        }
+        json.insert("bootstrap_accounts".to_owned(), Value::from(&accounts));
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Protocol, ProtocolConstants, ProtocolParameterBuilder};
+    use std::io::{Seek, Write};
+
+    use tempfile::NamedTempFile;
+
+    use super::{
+        BootstrapAccount, Protocol, ProtocolConstants, ProtocolParameterBuilder,
+    };
+
+    const ACCOUNT_PUBLIC_KEY: &str =
+        "edpkubRfnPoa6ts5vBdTB5syvjeK2AyEF3kyhG4Sx7F9pU3biku4wv";
+
+    fn create_dummy_source_file(
+        bootstrap_accounts: Option<Vec<BootstrapAccount>>,
+    ) -> NamedTempFile {
+        let mut source_file = tempfile::NamedTempFile::new().unwrap();
+        let mut json = serde_json::json!({"foo":"bar"});
+        if let Some(accounts) = bootstrap_accounts {
+            let obj = json.as_object_mut().unwrap();
+            obj.insert(
+                "bootstrap_accounts".to_owned(),
+                serde_json::Value::Array(
+                    accounts
+                        .iter()
+                        .map(|v| {
+                            serde_json::Value::Array(vec![
+                                serde_json::Value::String(v.get_public_key().to_string()),
+                                serde_json::Value::String(v.get_amount().to_string()),
+                            ])
+                        })
+                        .collect::<Vec<serde_json::Value>>(),
+                ),
+            );
+        }
+        serde_json::to_writer(source_file.as_file(), &json).unwrap();
+        source_file.flush().unwrap();
+        source_file.rewind().unwrap();
+        source_file
+    }
 
     #[test]
     fn parameter_builder() {
@@ -154,13 +228,18 @@ mod tests {
         builder
             .set_constants(ProtocolConstants::Sandbox)
             .set_protocol(Protocol::Alpha)
-            .set_source_path("/test/path");
+            .set_source_path("/test/path")
+            .set_bootstrap_accounts(
+                [BootstrapAccount::new(ACCOUNT_PUBLIC_KEY, 1000).unwrap()].to_vec(),
+            );
         assert_eq!(builder.constants.unwrap(), ProtocolConstants::Sandbox);
-        assert_eq!(
-            builder.source_path.unwrap().as_os_str().to_str().unwrap(),
-            "/test/path"
-        );
+        assert_eq!(builder.source_path.unwrap().to_str().unwrap(), "/test/path");
         assert_eq!(builder.protocol.unwrap().hash(), Protocol::Alpha.hash());
+        assert_eq!(builder.bootstrap_accounts.get_accounts().len(), 1);
+        assert_eq!(
+            builder.bootstrap_accounts.get_accounts().last().unwrap(),
+            &BootstrapAccount::new(ACCOUNT_PUBLIC_KEY, 1000).unwrap()
+        );
     }
 
     #[test]
@@ -174,15 +253,73 @@ mod tests {
     #[test]
     fn build_parameters_from_given_file() {
         let mut builder = ProtocolParameterBuilder::new();
-        let source_file = tempfile::NamedTempFile::new().unwrap();
+        let source_file = create_dummy_source_file(None);
         builder.set_source_path(source_file.path().to_str().unwrap());
-        let json = serde_json::json!({"foo":"bar"});
-        serde_json::to_writer(&source_file, &json).unwrap();
 
         let output_file = builder.build().unwrap();
         let json: serde_json::Value = serde_json::from_reader(output_file).unwrap();
 
         // this output file should have the values as the source file above
         assert_eq!(json.get("foo").unwrap().as_str().unwrap(), "bar");
+    }
+
+    #[test]
+    fn set_bootstrap_accounts() {
+        let mut builder = ProtocolParameterBuilder::new();
+        builder.set_bootstrap_accounts(
+            [BootstrapAccount::new(ACCOUNT_PUBLIC_KEY, 1000).unwrap()].to_vec(),
+        );
+        let output_file = builder.build().unwrap();
+        let json: serde_json::Value = serde_json::from_reader(output_file).unwrap();
+
+        let accounts = json.get("bootstrap_accounts").unwrap().as_array().unwrap();
+        assert_eq!(accounts.len(), 1);
+        let account = accounts.last().unwrap().as_array().unwrap();
+        assert_eq!(
+            account.get(0).unwrap().as_str().unwrap(),
+            ACCOUNT_PUBLIC_KEY
+        );
+        assert_eq!(account.get(1).unwrap().as_str().unwrap(), "1000");
+    }
+
+    #[test]
+    fn merge_existing_bootstrap_accounts() {
+        let mut builder = ProtocolParameterBuilder::new();
+        let source_file = create_dummy_source_file(Some(vec![BootstrapAccount::new(
+            "edpkvLEsnuq1TnYX9uc4Mcig9AiP7m3VtHpNGViDBivbvYwzzhzZRx",
+            1,
+        )
+        .unwrap()]));
+        builder
+            .set_source_path(&source_file.path().to_str().unwrap())
+            .set_bootstrap_accounts(
+                [BootstrapAccount::new(ACCOUNT_PUBLIC_KEY, 1000).unwrap()].to_vec(),
+            );
+        let output_file = builder.build().unwrap();
+        let json: serde_json::Value = serde_json::from_reader(output_file).unwrap();
+
+        let mut accounts = json
+            .get("bootstrap_accounts")
+            .unwrap()
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|v| BootstrapAccount::try_from(v).unwrap())
+            .collect::<Vec<BootstrapAccount>>();
+        assert_eq!(accounts.len(), 2);
+        accounts.sort_by_key(|v| v.get_amount());
+
+        // accounts sorted by tez in ascending order, so the first one is the existing account
+        // with 1 mutez only
+        let existing_account = accounts.first().unwrap();
+        assert_eq!(existing_account.get_amount(), 1);
+        assert_ne!(
+            existing_account.get_public_key().to_string(),
+            ACCOUNT_PUBLIC_KEY
+        );
+
+        let new_account = accounts.last().unwrap();
+        assert_eq!(new_account.get_amount(), 1000);
+        assert_eq!(new_account.get_public_key().to_string(), ACCOUNT_PUBLIC_KEY);
     }
 }
