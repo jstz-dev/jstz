0b372eec79e84e4b6d0bc3d88d1b901dc07188c8
Alistair <ajo41@cam.ac.uk>
2024-10-30 20:55:58 +0000

    feat: add `jstz_engine` crate
    Adds a simple dummy crate that uses `mozjs` as a dependency

---
+++ crates/jstz_engine/src/lib.rs
@@ -0,0 +1,56 @@
+pub fn hello() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+
+    use ::std::ptr;
+
+    use mozjs::jsapi::*;
+    use mozjs::jsval::UndefinedValue;
+    use mozjs::rooted;
+    use mozjs::rust::SIMPLE_GLOBAL_CLASS;
+    use mozjs::rust::{JSEngine, RealmOptions, Runtime};
+
+    #[test]
+    fn test_eval() {
+        let engine = JSEngine::init().unwrap();
+        let rt = Runtime::new(engine.handle());
+
+        let options = RealmOptions::default();
+        rooted!(in(rt.cx()) let global = unsafe {
+            JS_NewGlobalObject(rt.cx(), &SIMPLE_GLOBAL_CLASS, ptr::null_mut(),
+                               OnNewGlobalHookOption::FireOnNewGlobalHook,
+                               &*options)
+        });
+
+        /* These should indicate source location for diagnostics. */
+        let filename: &'static str = "inline.js";
+        let lineno: u32 = 1;
+
+        /*
+         * The return value comes back here. If it could be a GC thing, you must add it to the
+         * GC's "root set" with the rooted! macro.
+         */
+        rooted!(in(rt.cx()) let mut rval = UndefinedValue());
+
+        /*
+         * Some example source in a string. This is equivalent to JS_EvaluateScript in C++.
+         */
+        let source: &'static str = "40 + 2";
+
+        let res = rt.evaluate_script(
+            global.handle(),
+            source,
+            filename,
+            lineno,
+            rval.handle_mut(),
+        );
+
+        assert!(res.is_ok());
+        /* Should get a number back from the example source. */
+        assert!(rval.get().is_int32());
+        assert_eq!(rval.get().to_int32(), 42);
+    }
+}
