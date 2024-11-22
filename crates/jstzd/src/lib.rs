e12aa26b14f4cf003ecffe17fa264b68ac54650a
leo <leo.unoki@trili.tech>
2024-11-22 13:08:27 +0000

    feat(jstzd): add build script for rollup

--- crates/jstzd/src/lib.rs
+++ crates/jstzd/src/lib.rs
@@ -5,7 +5,7 @@ pub mod task;
 pub use config::BOOTSTRAP_CONTRACT_NAMES;
 use std::process::exit;
 
-pub const EXCHANGER_ADDRESS: &str = "KT1F3MuqvT9Yz57TgCS3EkDcKNZe9HpiavUJ";
+include!("../build_config.rs");
 pub const JSTZ_ROLLUP_ADDRESS: &str = "sr1PuFMgaRUN12rKQ3J2ae5psNtwCxPNmGNK";
 pub const JSTZ_NATIVE_BRIDGE_ADDRESS: &str = "KT1GFiPkkTjd14oHe6MrBPiRh5djzRkVWcni";
 
