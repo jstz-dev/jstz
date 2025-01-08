3c37873919c1cc408bd7080c7dbce2f6681cd7c1
Alistair <ajo41@cam.ac.uk>
2025-01-08 15:21:14 +0000

    refactor(jstz_engine): replace existing wrapper definitions with common `gcptr_wrapper` macro

--- crates/jstz_engine/src/gc/mod.rs
+++ crates/jstz_engine/src/gc/mod.rs
@@ -27,3 +27,82 @@ pub mod trace;
 pub use compartment::Compartment;
 pub use root::Prolong;
 pub use trace::{Finalize, Trace, Tracer};
+
+#[macro_export]
+macro_rules! gcptr_wrapper {
+    ($doc:expr, $name: ident, $ptr_type: ty) => {
+        #[doc = $doc]
+        pub struct $name<'a, C: $crate::gc::Compartment> {
+            inner_ptr: std::pin::Pin<std::sync::Arc<$crate::gc::ptr::GcPtr<$ptr_type>>>,
+            marker: std::marker::PhantomData<(&'a (), C)>,
+        }
+
+        impl<'a, C: $crate::gc::Compartment> Clone for $name<'a, C> {
+            fn clone(&self) -> Self {
+                Self {
+                    inner_ptr: self.inner_ptr.clone(),
+                    marker: self.marker,
+                }
+            }
+        }
+
+        impl<'a, C: $crate::gc::Compartment> $crate::gc::ptr::AsRawPtr for $name<'a, C> {
+            type Ptr = $ptr_type;
+
+            unsafe fn as_raw_ptr(&self) -> Self::Ptr {
+                self.inner_ptr.as_raw_ptr()
+            }
+        }
+
+        impl<'a, C: $crate::gc::Compartment> $crate::gc::ptr::AsRawHandle
+            for $name<'a, C>
+        {
+            unsafe fn as_raw_handle(&self) -> $crate::gc::ptr::Handle<Self::Ptr> {
+                self.inner_ptr.as_raw_handle()
+            }
+        }
+
+        impl<'a, C: $crate::gc::Compartment> $crate::gc::ptr::AsRawHandleMut
+            for $name<'a, C>
+        {
+            unsafe fn as_raw_handle_mut(&self) -> $crate::gc::ptr::HandleMut<Self::Ptr> {
+                self.inner_ptr.as_raw_handle_mut()
+            }
+        }
+
+        impl<'a, C: $crate::gc::Compartment> $name<'a, C> {
+            /// Converts a raw pointer to the wrapped type.
+            ///
+            /// # Safety
+            ///
+            /// If the pointer does not correctly point to a valid GC object (of the expected type)
+            /// then `TypeError`s, segmentation faults, or Undefined Behaviour in general could
+            /// occur.
+            #[allow(dead_code)]
+            pub(crate) unsafe fn from_raw(ptr: $ptr_type) -> Self {
+                Self {
+                    inner_ptr: $crate::gc::ptr::GcPtr::pinned(ptr),
+                    marker: std::marker::PhantomData,
+                }
+            }
+        }
+
+        unsafe impl<'a, 'b, C: $crate::gc::Compartment> $crate::gc::Prolong<'a>
+            for $name<'b, C>
+        {
+            type Aged = $name<'a, C>;
+        }
+
+        impl<'a, C: $crate::gc::Compartment> $crate::gc::Finalize for $name<'a, C> {
+            fn finalize(&self) {
+                self.inner_ptr.finalize()
+            }
+        }
+
+        unsafe impl<'a, C: $crate::gc::Compartment> $crate::gc::Trace for $name<'a, C> {
+            $crate::custom_trace!(this, mark, {
+                mark(&this.inner_ptr);
+            });
+        }
+    };
+}
