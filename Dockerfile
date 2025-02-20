e962b965d4f529b09ebbfe97a976dd80a7acbd23
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-02-20 10:37:31 +0000

    test:test

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,16 @@ RUN echo 'deb [arch=riscv64] http://ports.ubuntu.com/ubuntu-ports noble main uni
     echo 'deb [arch=riscv64] http://ports.ubuntu.com/ubuntu-ports noble-security main universe multiverse restricted' >> /etc/apt/sources.list
 RUN dpkg --add-architecture riscv64
 # add for actions
-RUN apt update && apt install -y curl git && apt-get clean
+RUN apt update && apt install -y curl git wget && apt-get clean
 # add main build deps
 RUN apt install --no-install-recommends -y build-essential pkg-config m4 python3 python3-setuptools llvm llvm-dev lld libclang-dev clang && apt-get clean
 # add cross deps
 RUN apt install --no-install-recommends -y gcc-riscv64-linux-gnu g++-riscv64-linux-gnu qemu-user qemu-user-static && apt-get clean
 # add runtime deps
 RUN apt install --no-install-recommends -y libc6:riscv64 libstdc++6:riscv64 && apt-get clean
+RUN find "/usr/bin/" -name "riscv64-linux-gnu*" -exec sh -c 't=$(basename $0 | sed -e "s/riscv64-linux-gnu/riscv64gc-unknown-linux-gnu/g"); ln -s "/usr/bin/$(basename $0)" "/usr/bin/$t"' {} \;
 
-RUN sh -c 'for v in $(ls "/usr/bin" | grep "riscv64-linux-gnu"); do t=$(echo "$v" | sed -e "s/riscv64-linux-gnu/riscv64gc-unknown-linux-gnu/g"); ln -s "/usr/bin/$v" "/usr/bin/$t" ; done'
+ENV PATH="$PATH:/tmp/riscv64-linux-musl-cross/bin"
+
+RUN dst="/tmp/riscv64-linux-musl-cross" && wget -O /tmp/riscv64-linux-musl-cross.tgz https://musl.cc/riscv64-linux-musl-cross.tgz && tar -xf /tmp/riscv64-linux-musl-cross.tgz -C /tmp && rm https://musl.cc/riscv64-linux-musl-cross.tgz && find "$dst/bin/" -name "riscv64-linux*" -exec sh -c 't=$(basename $1 | sed -e "s/riscv64-linux-musl/riscv64gc-unknown-linux-musl/g"); ln -s "$0/$(basename $1)" "$0/$t"' "$dst/bin" {} \; && ln -s $dst/riscv64-linux-musl/lib/libc.so /lib/ld-musl-riscv64.so.1
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && . "$HOME/.cargo/env" && rustup toolchain install 1.82.0 && rustup target add --toolchain 1.82.0 riscv64gc-unknown-linux-musl
--- build-mozjs.sh
+++ build-mozjs.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-apt install -y wget
-dst="/tmp/riscv64-linux-musl-cross"
-wget -O /tmp/riscv64-linux-musl-cross.tgz https://musl.cc/riscv64-linux-musl-cross.tgz && tar -xf /tmp/riscv64-linux-musl-cross.tgz -C /tmp
-for v in $(ls "$dst/bin" | grep "riscv64-linux"); do t=$(echo "$v" | sed -e "s/riscv64-linux-musl/riscv64gc-unknown-linux-musl/g"); ln -s "$dst/bin/$v" "$dst/bin/$t" ; done
-export PATH=$PATH:$dst/bin
-git clone https://github.com/servo/mozjs.git --branch mozjs-sys-v0.128.6-1 --single-branch
+
+git clone https://github.com/servo/mozjs.git --branch mozjs-sys-v0.128.3-0 --single-branch
 cd mozjs && cat mozjs-sys/mozjs/build/autoconf/config.sub | sed 's/riscv64 | riscv64be/riscv64 | riscv64be | riscv64gc/g' > a && mv a mozjs-sys/mozjs/build/autoconf/config.sub
-rustup target add --toolchain 1.82.0 riscv64gc-unknown-linux-musl
-env MOZJS_CREATE_ARCHIVE=1 BINDGEN_EXTRA_CLANG_ARGS='-target riscv64-unknown-linux-musl -I/usr/include/riscv64-linux-musl --sysroot=/ -Wno-unused-command-line-argument -fuse-ld=lld' CLANGFLAGS="-target riscv64-unknown-linux-musl -I/usr/include/riscv64-linux-musl --sysroot=/ -Wno-unused-command-line-argument -fuse-ld=lld" CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_RUNNER="/usr/bin/qemu-riscv64-static" CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_LINKER="/tmp/riscv64-linux-musl-cross/bin/riscv64-linux-musl-gcc" CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_CC="/tmp/riscv64-linux-musl-cross/bin/riscv64-linux-musl-gcc" CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_CXX="/tmp/riscv64-linux-musl-cross/bin/riscv64-linux-musl-g++" CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_AR="/tmp/riscv64-linux-musl-cross/bin/riscv64-linux-musl-ar" MOZJS_FROM_SOURCE=1 cargo +1.82.0 build --target=riscv64gc-unknown-linux-musl
+
+env MOZJS_CREATE_ARCHIVE=1 CLANGFLAGS="-target riscv64-unknown-linux-musl --sysroot=/ -Wno-unused-command-line-argument -fuse-ld=lld" BINDGEN_EXTRA_CLANG_ARGS='-target riscv64-unknown-linux-musl --sysroot=/ -Wno-unused-command-line-argument -fuse-ld=lld' CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_RUNNER="/usr/bin/qemu-riscv64-static" CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_LINKER="riscv64-linux-musl-gcc" CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_AR="riscv64-linux-musl-ar" MOZJS_FROM_SOURCE=1 cargo +1.82.0 build --target=riscv64gc-unknown-linux-musl
