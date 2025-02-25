a18e8cd6226ffdbedaae7f0188122bb25bf849b1
Tim McMackin <timothymcmackin@yahoo.com>
2025-02-25 15:18:03 -0500

    feat(examples): example of calling from a web application

---
+++ examples/call-from-web/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import wasm from "vite-plugin-wasm";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), wasm()],
+  build: {
+    target: "esnext",
+  },
+});
