c275b04d5ee3a50d0eaaebac29d753b8ba301764
Ryan Tan <excalcabter@gmail.com>
2025-03-12 02:03:18 +0000

    feat(runtime): add JstzRuntime

---
+++ crates/jstz_proto/src/runtime/mod.rs
@@ -0,0 +1,55 @@
+use jstz_core::{
+    host::{HostRuntime, JsHostRuntime},
+    kv::Transaction,
+};
+use jstz_crypto::{hash::Hash, smart_function_hash::SmartFunctionHash};
+use jstz_runtime::JstzRuntime;
+
+use crate::api::Kv;
+
+/// Initializes a new [`JstzRuntime`] with the given protocol
+pub fn init_jstz_runtime(protocol: Protocol) -> JstzRuntime {
+    let extensions = vec![];
+    JstzRuntime::init(extensions, Some(protocol))
+}
+
+/// [`Protocol`] exposes proto-specific state to [`JstzRuntime`]
+pub struct Protocol {
+    pub host: JsHostRuntime<'static>,
+    pub tx: &'static mut Transaction,
+    pub kv: Kv,
+}
+
+impl Protocol {
+    pub fn new(
+        hrt: &mut impl HostRuntime,
+        tx: &mut Transaction,
+        address: SmartFunctionHash,
+    ) -> Self {
+        let host = JsHostRuntime::new(hrt);
+        // Safety: Since we synchronisely execute Operatios, the tx will not be dropped before
+        // the runtime, so this is safe
+        let tx = unsafe { std::mem::transmute(tx) };
+        let kv = Kv::new(address.to_base58());
+        Protocol { host, tx, kv }
+    }
+}
+
+#[cfg(test)]
+mod test_utils {
+
+    use tezos_smart_rollup_mock::MockHost;
+
+    #[allow(unused)]
+    pub fn init_mock_host() -> (Box<Vec<u8>>, MockHost) {
+        let mut sink: Box<Vec<u8>> = Box::default();
+        let mut host = MockHost::default();
+        host.set_debug_handler(unsafe {
+            std::mem::transmute::<&mut std::vec::Vec<u8>, &'static mut Vec<u8>>(
+                sink.as_mut(),
+            )
+        });
+
+        (sink, host)
+    }
+}
---
+++ crates/jstz_runtime/src/runtime.rs
@@ -0,0 +1,124 @@
+use deno_core::*;
+use serde::Deserialize;
+use std::ops::Deref;
+use std::ops::DerefMut;
+
+fn init_extenions() -> Vec<Extension> {
+    vec![]
+}
+
+/// [`JstzRuntime`] manages the [`JsRuntime`] state. It is also
+/// provides [`JsRuntime`] with the instiatiated [`HostRuntime`]
+/// and protocol capabilities
+pub struct JstzRuntime {
+    runtime: JsRuntime,
+}
+
+pub struct JstzRuntimeSnapshot(Box<[u8]>);
+impl JstzRuntimeSnapshot {
+    pub fn snapshot(self) -> &'static [u8] {
+        // Safety: `JstzRuntimeSnapshot` is only dropped when the kernel
+        // is shutdown
+        Box::leak(self.0)
+    }
+
+    pub fn new(options: RuntimeOptions) -> Self {
+        let snapshot = JsRuntimeForSnapshot::new(options);
+        Self(snapshot.snapshot())
+    }
+}
+
+impl JstzRuntime {
+    /// Returns the default [`RuntimeOptions`] configured
+    /// with custom extensions
+    pub fn options() -> RuntimeOptions {
+        let extensions = init_extenions();
+        RuntimeOptions {
+            extensions,
+            ..Default::default()
+        }
+    }
+
+    /// Creates a new [`JstzRuntime`] with protocol extensions registered
+    /// and an instance of Protocol exposed in the [`OpState`]
+    pub fn init<Protocol: 'static>(
+        protocol_ext: Vec<Extension>,
+        protocol: Option<Protocol>,
+    ) -> Self {
+        let mut extensions = init_extenions();
+        extensions.extend(protocol_ext);
+
+        let mut runtime = JsRuntime::new(RuntimeOptions {
+            extensions,
+            ..Default::default()
+        });
+
+        if let Some(protocol) = protocol {
+            let op_state = runtime.op_state();
+            op_state.borrow_mut().put(protocol);
+        };
+
+        Self { runtime }
+    }
+
+    /// Creates a [`JstzRuntime`] from a snapshot blob
+    // FIXME: Doesn't work as expected
+    pub fn from_snapshot<Protocol: 'static>(
+        snapshot: &'static [u8],
+        protocol: Option<Protocol>,
+    ) -> Self {
+        let mut runtime = JsRuntime::new(RuntimeOptions {
+            startup_snapshot: Some(snapshot),
+            ..Default::default()
+        });
+
+        if let Some(protocol) = protocol {
+            let op_state = runtime.op_state();
+            op_state.borrow_mut().put(protocol);
+        };
+
+        Self { runtime }
+    }
+
+    /// Executes traditional, non-ECMAScript-module JavaScript code, ignoring
+    /// its result
+    pub fn execute(mut self, code: &str) -> Option<()> {
+        self.execute_script("jstz://run", code.to_string()).ok()?;
+        Some(())
+    }
+
+    /// Executes traditional, non-ECMAScript-module JavaScript code, parsing
+    /// its result ot a Rust type T
+    pub fn execute_with_result<'de, T: Deserialize<'de>>(
+        &mut self,
+        code: &str,
+    ) -> Option<T> {
+        let value = self.execute_script("jstz://run", code.to_string()).unwrap();
+        let scope = &mut self.handle_scope();
+        let local = v8::Local::new(scope, value);
+        serde_v8::from_v8::<T>(scope, local).ok()
+    }
+}
+
+impl Deref for JstzRuntime {
+    type Target = JsRuntime;
+
+    fn deref(&self) -> &Self::Target {
+        &self.runtime
+    }
+}
+
+impl DerefMut for JstzRuntime {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.runtime
+    }
+}
+
+#[macro_export]
+macro_rules! init_ops_and_esm_extensions  {
+    ($($ext:ident),*) => {
+        vec![
+            $($ext::init_ops_and_esm()),*
+        ]
+    };
+}
