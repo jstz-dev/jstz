17512997603476d7833fd7f6d96653132c180d03
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-03-18 14:29:50 +0000

    feat(runtime): wpt for jstz_runtime

---
+++ crates/jstz_runtime/tests/test_harness_api.js
@@ -0,0 +1,31 @@
+import { test_result_callback, test_completion_callback } from "ext:core/ops";
+
+Object.defineProperty(globalThis, "location", {
+  value: {
+    search: "?0-5",
+  },
+  enumerable: true,
+  configurable: true,
+  writable: true,
+});
+
+Object.defineProperty(globalThis, "self", {
+  value: globalThis,
+  enumerable: true,
+  configurable: true,
+  writable: true,
+});
+
+Object.defineProperty(globalThis, "test_result_callback", {
+  value: test_result_callback,
+  enumerable: true,
+  configurable: true,
+  writable: true,
+});
+
+Object.defineProperty(globalThis, "test_completion_callback", {
+  value: test_completion_callback,
+  enumerable: true,
+  configurable: true,
+  writable: true,
+});
