9c9dbe7b25b51ead0d47a63b20eddab8531e0803
Alistair <ajo41@cam.ac.uk>
2025-03-19 15:31:17 +0000

    feat(runtime): add support for timers, Encoding, File and Streams APIs

--- crates/jstz_runtime/src/ext/jstz_main/mod.rs
+++ crates/jstz_runtime/src/ext/jstz_main/mod.rs
@@ -2,7 +2,7 @@ use deno_core::extension;
 
 extension!(
   jstz_main,
-  deps = [deno_webidl, deno_console, jstz_console, deno_url],
+  deps = [deno_webidl, deno_console, jstz_console, deno_url, deno_web],
   esm_entry_point = "ext:jstz_main/99_main.js",
   esm = [dir "src/ext/jstz_main", "98_global_scope.js", "99_main.js"],
 );
