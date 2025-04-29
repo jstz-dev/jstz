4804f79f43d7cc659a16dc6058655e6f232e9a0f
Ryan Tan <excalcabter@gmail.com>
2025-04-29 18:23:30 +0100

    feat(proto): add fetch support

--- crates/jstz_proto/src/runtime/mod.rs
+++ crates/jstz_proto/src/runtime/mod.rs
@@ -1,3 +1,6 @@
 mod v1;
 
 pub use v1::{Kv, KvValue, ProtocolApi, ProtocolData};
+
+#[cfg(feature = "riscv")]
+pub mod v2;
---
+++ crates/jstz_proto/src/runtime/v2/tests/resources/transfer_fails_when_error_thrown/remote.js
@@ -0,0 +1,4 @@
+/** @param {Request} req */
+export default async (_req) => {
+  throw new Error("Boom");
+};
