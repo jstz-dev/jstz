e033fffaf915deb3f57243c0dbfa78bd0389742c
Ryan Tan <excalcabter@gmail.com>
2025-05-20 09:58:04 +0100

    refactor(proto): move proto api to v1

--- crates/jstz_proto/src/api/mod.rs
+++
@@ -1,7 +0,0 @@
-mod kv;
-mod ledger;
-mod smart_function;
-
-pub use kv::{Kv, KvApi, KvValue};
-pub use ledger::LedgerApi;
-pub use smart_function::{SmartFunctionApi, TraceData};
--- crates/jstz_proto/src/lib.rs
+++ crates/jstz_proto/src/lib.rs
@@ -1,4 +1,3 @@
-pub mod api;
 mod error;
 
 pub mod context;
@@ -8,3 +7,5 @@ pub mod operation;
 pub mod receipt;
 pub mod request_logger;
 pub use error::{Error, Result};
+
+pub mod runtime;
