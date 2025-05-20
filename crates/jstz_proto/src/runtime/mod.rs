c5a798b176895fe999be01b4517d8b35fe5a1f98
Ryan Tan <excalcabter@gmail.com>
2025-05-20 10:45:16 +0100

    refactor(proto): move js logger to v1

--- crates/jstz_proto/src/runtime/mod.rs
+++ crates/jstz_proto/src/runtime/mod.rs
@@ -1,6 +1,6 @@
 pub mod v1;
 
-pub use v1::{Kv, KvValue, ParsedCode, ProtocolData};
+pub use v1::{Kv, KvValue, LogRecord, ParsedCode, ProtocolData, LOG_PREFIX};
 
 #[cfg(feature = "riscv")]
 pub mod v2;
