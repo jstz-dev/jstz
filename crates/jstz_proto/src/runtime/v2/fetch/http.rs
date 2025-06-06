b413ca604eedf6956b647ff6a5e116328d8d240f
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-06-06 15:02:59 +0100

    feat(proto): convert v2 http response

--- crates/jstz_proto/src/runtime/v2/fetch/http.rs
+++ crates/jstz_proto/src/runtime/v2/fetch/http.rs
@@ -1,4 +1,5 @@
 use deno_core::{ByteString, JsBuffer};
+use http::{HeaderMap, HeaderName, HeaderValue};
 use url::Url;
 
 use crate::context::account::Address;
@@ -12,6 +13,37 @@ pub struct Response {
     pub status_text: String,
     pub headers: Vec<(ByteString, ByteString)>,
     pub body: Body,
+}
+
+impl Response {
+    pub fn to_http_response(self) -> http::Response<Option<Vec<u8>>> {
+        let mut builder = http::Response::builder().status(self.status);
+
+        let headers =
+            HeaderMap::from_iter(self.headers.into_iter().map(|(key, value)| {
+                let key_vec: Vec<u8> = key.into();
+                let value_vec: Vec<u8> = value.into();
+                (
+                    HeaderName::from_bytes(&key_vec)
+                        .expect("Expected valid http header key from a valid response"),
+                    HeaderValue::from_bytes(&value_vec)
+                        .expect("Expected valid http header value from a valid response"),
+                )
+            }));
+
+        *builder.headers_mut().unwrap() = headers;
+
+        let body_vec = self.body.to_vec();
+        let body = if body_vec.is_empty() {
+            None
+        } else {
+            Some(body_vec)
+        };
+
+        builder
+            .body(body)
+            .expect("Expected valid http response from a valid response")
+    }
 }
 
 #[derive(Debug)]
@@ -62,5 +94,63 @@ impl TryFrom<&Url> for Address {
         let raw_address = url.host().ok_or(url::ParseError::EmptyHost)?;
         Address::from_base58(raw_address.to_string().as_str())
             .map_err(|err| FetchError::JstzError(err.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use http::{HeaderMap, HeaderName, HeaderValue};
+
+    #[test]
+    fn response_to_http_response() {
+        let response = super::Response {
+            status: 403,
+            status_text: "something".to_string(),
+            headers: vec![
+                ("k1".as_bytes().into(), "v1".as_bytes().into()),
+                ("k2".as_bytes().into(), "v3".as_bytes().into()),
+                ("k2".as_bytes().into(), "v2".as_bytes().into()),
+                ("k1".as_bytes().into(), "v4".as_bytes().into()),
+            ],
+            body: super::Body::Vector(vec![1, 2, 3]),
+        };
+        let http_response = response.to_http_response();
+        let (parts, body) = http_response.into_parts();
+        assert_eq!(body, Some(vec![1, 2, 3]));
+        assert_eq!(parts.status, http::StatusCode::FORBIDDEN);
+        let mut expected_headers = HeaderMap::new();
+        // Ordering doesn't matter; HeaderMap handles it during comparison.
+        // The key is that our implementation also allows duplicated keys.
+        expected_headers.append(
+            HeaderName::from_static("k1"),
+            HeaderValue::from_static("v1"),
+        );
+        expected_headers.append(
+            HeaderName::from_static("k2"),
+            HeaderValue::from_static("v3"),
+        );
+        expected_headers.append(
+            HeaderName::from_static("k2"),
+            HeaderValue::from_static("v2"),
+        );
+        expected_headers.append(
+            HeaderName::from_static("k1"),
+            HeaderValue::from_static("v4"),
+        );
+        assert_eq!(parts.headers.len(), 4);
+        assert_eq!(parts.headers, expected_headers);
+    }
+
+    #[test]
+    fn response_to_http_response_empty_body() {
+        let response = super::Response {
+            status: 401,
+            status_text: "something".to_string(),
+            headers: vec![],
+            body: super::Body::Vector(vec![]),
+        };
+        let http_response = response.to_http_response();
+        let body = http_response.into_body();
+        assert_eq!(body, None);
     }
 }
