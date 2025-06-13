885683b31877a064f587e2c5e5625c8ce5cbb838
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-06-13 10:54:52 +0100

    feat(proto): log requests with host

--- crates/jstz_proto/src/lib.rs
+++ crates/jstz_proto/src/lib.rs
@@ -8,3 +8,33 @@ pub mod request_logger;
 pub use error::{Error, Result};
 
 pub mod runtime;
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
+
+    use tezos_smart_rollup_mock::DebugSink;
+
+    pub struct DebugLogSink {
+        pub inner: Arc<Mutex<Vec<u8>>>,
+    }
+
+    impl DebugSink for DebugLogSink {
+        fn write_all(&mut self, buffer: &[u8]) -> std::io::Result<()> {
+            self.inner.lock().unwrap().extend_from_slice(buffer);
+            Ok(())
+        }
+    }
+
+    impl DebugLogSink {
+        pub fn new() -> Self {
+            Self {
+                inner: Arc::new(Mutex::new(vec![])),
+            }
+        }
+
+        pub fn content(&self) -> Arc<Mutex<Vec<u8>>> {
+            self.inner.clone()
+        }
+    }
+}
