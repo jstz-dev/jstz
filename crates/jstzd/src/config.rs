02c9580b9545f130ecb3642454386ab1aaf147cc
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-06-27 12:08:08 +0100

    chore(jstzd): create dedicated account for rollup operator

--- crates/jstzd/src/config.rs
+++ crates/jstzd/src/config.rs
@@ -38,7 +38,7 @@ pub const BOOTSTRAP_CONTRACT_NAMES: [(&str, &str); 2] = [
     ("exchanger", EXCHANGER_ADDRESS),
     ("jstz_native_bridge", JSTZ_NATIVE_BRIDGE_ADDRESS),
 ];
-pub const ROLLUP_OPERATOR_ACCOUNT_ALIAS: &str = "bootstrap1";
+pub(crate) const ROLLUP_OPERATOR_ACCOUNT_ALIAS: &str = "rollup_operator";
 pub(crate) const ACTIVATOR_ACCOUNT_ALIAS: &str = "activator";
 pub(crate) const INJECTOR_ACCOUNT_ALIAS: &str = "injector";
 
@@ -804,7 +804,7 @@ mod tests {
         assert_eq!(contracts.len(), 2);
 
         let accounts = read_bootstrap_accounts_from_param_file(&config_path).await;
-        assert_eq!(accounts.len(), 8);
+        assert_eq!(accounts.len(), 9);
 
         assert_eq!(
             config.octez_rollup_config().address.to_base58_check(),
@@ -857,7 +857,7 @@ mod tests {
             .unwrap()
             .as_array()
             .unwrap();
-        assert_eq!(accounts.len(), 7);
+        assert_eq!(accounts.len(), 8);
 
         let bootstrap_accounts = accounts
             .iter()
