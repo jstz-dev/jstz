7865c8422704cbbfed74823ccfd884339cac916f
Ryan Tan <excalcabter@gmail.com>
2025-07-02 03:00:49 +0200

    fix(oracle): oracle use injector key by default

--- crates/jstz_proto/src/runtime/v2/protocol_context.rs
+++ crates/jstz_proto/src/runtime/v2/protocol_context.rs
@@ -3,6 +3,7 @@ use std::sync::{Arc, OnceLock};
 use jstz_core::{host::HostRuntime, kv::Storage};
 use jstz_crypto::public_key::PublicKey;
 use parking_lot::Mutex;
+use tezos_smart_rollup::storage::path::RefPath;
 
 use crate::{storage::ORACLE_PUBLIC_KEY_PATH, BlockLevel};
 
@@ -45,10 +46,19 @@ impl ProtocolContext {
         // FIXME(https://linear.app/tezos/issue/JSTZ-746/make-oracle-pk-configurable)
         // Make configurable
         // Hardcode oracle value pk to injector pk for now
-        let oracle_key = PublicKey::from_base58(
-            "edpkuBknW28nW72KG6RoHtYW7p12T6GKc7nAbwYX5m8Wd9sDVC9yav",
-        )
-        .unwrap();
+        let oracle_key = if cfg!(test) {
+            // Hard code in tests. Remove later by always Initializing store
+            // with an injector
+            PublicKey::from_base58(
+                "edpkuBknW28nW72KG6RoHtYW7p12T6GKc7nAbwYX5m8Wd9sDVC9yav",
+            )
+            .unwrap()
+        } else {
+            Storage::get(rt, &RefPath::assert_from(b"/injector"))
+                .ok()
+                .flatten()
+                .expect("Injector not found")
+        };
         Storage::insert(rt, &ORACLE_PUBLIC_KEY_PATH, &oracle_key).unwrap();
         let current_level = Arc::new(Mutex::new(current_level));
         let oracle = Oracle::new(rt, None)?;
