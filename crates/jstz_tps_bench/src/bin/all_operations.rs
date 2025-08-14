3e5e472b2738ac885a3079505a5781dc94fa6c52
huancheng-trili <168652455+huancheng-trili@users.noreply.github.com>
2025-08-14 13:26:50 +0100

    feat(bench): implement withdraw (#1270)

--- crates/jstz_tps_bench/src/bin/all_operations.rs
+++ crates/jstz_tps_bench/src/bin/all_operations.rs
@@ -91,6 +91,9 @@ export default async (request) => {{
         HttpBody::empty(),
     )?;
 
+    let receiver = accounts[0].address.clone();
+    builder.withdraw(&mut accounts[0], &receiver, 1)?;
+
     let inbox = builder.build();
     inbox.save(&args.inbox_file)
 }
