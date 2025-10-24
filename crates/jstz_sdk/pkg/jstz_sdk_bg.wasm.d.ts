3def89718e9cc63ea0f298d271f0e367c8522354
Ryan Tan <excalcabter@gmail.com>
2025-10-24 09:11:05 +0100

    feat: temporarily include jstz_sdk artfacts for testing

---
+++ crates/jstz_sdk/pkg/jstz_sdk_bg.wasm.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const sign_operation: (
+  a: any,
+  b: number,
+  c: number,
+) => [number, number, number, number];
+export const hash_operation: (a: any) => [number, number, number, number];
+export const parse_passkey_signature: (
+  a: any,
+) => [number, number, number, number];
+export const __getrandom_custom: (a: number, b: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
